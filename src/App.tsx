25946a304db57d4f5451c4ccfe66ad4446edc31b
Chris Casey <cdcasey@gmail.com>
2019-08-28 19:14:31 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Button} from '@material-ui/core'
 
-import Hello from './components/Hello'
+import Hello from 'components/Hello'
 
 
 const App: React.FC = () => {
+  function handleClick(event:React.MouseEvent): void {
+    console.log('button clicked', typeof event);
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <Hello name='Chris' enthusiasmLevel={1} />
+      <Button onClick={handleClick} variant="contained" title='Click it'>
+        Click it
+      </Button>
     </div>
   );
 }
